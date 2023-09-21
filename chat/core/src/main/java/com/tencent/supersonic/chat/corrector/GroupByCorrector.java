776158568d0b7dff41d351b34d1b9068c070726d
lexluo <lexluo@tencent.com>
2023-09-21 21:55:50 +0800

    [improvement][chat]llm parser corrector is simplified by sql distribution

---
+++ chat/core/src/main/java/com/tencent/supersonic/chat/corrector/GroupByCorrector.java
@@ -0,0 +1,15 @@
+package com.tencent.supersonic.chat.corrector;
+
+import com.tencent.supersonic.chat.api.pojo.SemanticCorrectInfo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GroupByCorrector extends BaseSemanticCorrector {
+
+    @Override
+    public void correct(SemanticCorrectInfo semanticCorrectInfo) {
+
+
+
+    }
+}
