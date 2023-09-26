ec3ebfd08b24916f2facca920c7640ddafcb6bbe
liangbowen <liangbowen@gf.com.cn>
2023-09-26 11:01:09 +0800

    support PIP_PATH and PYTHON_PATH env var

--- assembly/bin/supersonic-daemon.sh
+++ assembly/bin/supersonic-daemon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python_path="/usr/local/bin/python3"
+python_path=${PYTHON_PATH:-"/usr/local/bin/python3"}
 readonly CHAT_APP_NAME="supersonic_chat"
 readonly SEMANTIC_APP_NAME="supersonic_semantic"
 readonly LLMPARSER_APP_NAME="supersonic_llmparser"
