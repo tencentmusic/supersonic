de4fcb17117f8db650a57160ed0ad4a780f0a673
zuopengge <hwzuopengge@tencent.com>
2023-10-23 14:46:37 +0800

    [improvement] add traceid

--- chat/core/src/main/java/com/tencent/supersonic/chat/rest/ChatController.java
+++ chat/core/src/main/java/com/tencent/supersonic/chat/rest/ChatController.java
@@ -88,14 +88,14 @@ public class ChatController {
     }
 
     @RequestMapping("/getSolvedQuery")
-    public List<SolvedQueryRecallResp> getSolvedQuery(@RequestParam(value = "queryText") String queryText,
+    public QueryRecallResp getSolvedQuery(@RequestParam(value = "queryText") String queryText,
                                                       @RequestParam(value = "agentId") Integer agentId) {
         QueryRecallResp queryRecallResp = new QueryRecallResp();
         Long startTime = System.currentTimeMillis();
         List<SolvedQueryRecallResp> solvedQueryRecallRespList = chatService.getSolvedQuery(queryText, agentId);
         queryRecallResp.setSolvedQueryRecallRespList(solvedQueryRecallRespList);
         queryRecallResp.setQueryTimeCost(System.currentTimeMillis() - startTime);
-        return solvedQueryRecallRespList;
+        return queryRecallResp;
     }
 
 }
