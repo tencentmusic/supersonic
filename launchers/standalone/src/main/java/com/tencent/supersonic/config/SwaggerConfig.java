5102a6712bb1be09e926581b085ce3ef4cec7dca
jolunoluo <jolunoluo@tencent.com>
2023-11-02 18:56:50 +0800

    (fix)(config) fix SwaggerConfig checkstyle

--- launchers/standalone/src/main/java/com/tencent/supersonic/config/SwaggerConfig.java
+++ launchers/standalone/src/main/java/com/tencent/supersonic/config/SwaggerConfig.java
@@ -11,12 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-  @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
-  }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+              .select()
+              .apis(RequestHandlerSelectors.any())
+              .paths(PathSelectors.any())
+              .build();
+    }
 }
