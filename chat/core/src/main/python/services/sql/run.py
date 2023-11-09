41aa6ff8e40882cf4ca114b4a375c44ccc2276ac
codescracker <gswgdanb@hotmail.com>
2023-11-09 11:46:56 +0800

    add api service for sql_agent for crud opereations of few-shots examples. (#342)

--- chat/core/src/main/python/services/sql/run.py
+++ chat/core/src/main/python/services/sql/run.py
@@ -17,7 +17,7 @@ from instances.text2vec import Text2VecEmbeddingFunction
 from instances.chromadb_instance import client
 from instances.logging_instance import logger
 
-from few_shot_example.sql_exampler import examplars as sql_examplars
+from few_shot_example.sql_examplar import examplars as sql_examplars
 from config.config_parse import (TEXT2DSLAGENT_COLLECTION_NAME, TEXT2DSLAGENTCS_COLLECTION_NAME, 
                     TEXT2DSL_EXAMPLE_NUM, TEXT2DSL_FEWSHOTS_NUM, TEXT2DSL_SELF_CONSISTENCY_NUM,
                     TEXT2DSL_IS_SHORTCUT, TEXT2DSL_IS_SELF_CONSISTENCY)
@@ -32,12 +32,10 @@ text2dsl_agentcs_collection = client.get_or_create_collection(name=TEXT2DSLAGENT
                                             metadata={"hnsw:space": "cosine"})
 
 text2dsl_agent_example_prompter = FewShotPromptTemplate2(collection=text2dsl_agent_collection,
-                                            few_shot_examples=sql_examplars,
                                             retrieval_key="question",
                                             few_shot_seperator='\n\n')
 
 text2dsl_agentcs_example_prompter = FewShotPromptTemplate2(collection=text2dsl_agentcs_collection,
-                                            few_shot_examples=sql_examplars,
                                             retrieval_key="question",
                                             few_shot_seperator='\n\n')
 
@@ -47,9 +45,9 @@ text2sql_agent = Text2DSLAgent(num_fewshots=TEXT2DSL_EXAMPLE_NUM,
 text2sql_cs_agent = Text2DSLAgentConsistency(num_fewshots=TEXT2DSL_FEWSHOTS_NUM, num_examples=TEXT2DSL_EXAMPLE_NUM, num_self_consistency=TEXT2DSL_SELF_CONSISTENCY_NUM,
                                             sql_example_prompter=text2dsl_agentcs_example_prompter, llm=llm)
 
-text2sql_agent.update_examples(sql_examplars, TEXT2DSL_EXAMPLE_NUM)
-
-text2sql_cs_agent.update_examples(sql_examplars, TEXT2DSL_EXAMPLE_NUM, TEXT2DSL_FEWSHOTS_NUM, TEXT2DSL_SELF_CONSISTENCY_NUM)
+sql_ids = [str(i) for i in range(0, len(sql_examplars))]
+text2sql_agent.reload_setting(sql_ids, sql_examplars, TEXT2DSL_EXAMPLE_NUM)
+text2sql_cs_agent.reload_setting(sql_ids, sql_examplars, TEXT2DSL_EXAMPLE_NUM, TEXT2DSL_FEWSHOTS_NUM, TEXT2DSL_SELF_CONSISTENCY_NUM)
 
 
 text2sql_agent_router = Text2DSLAgentWrapper(sql_agent=text2sql_agent, sql_agent_cs=text2sql_cs_agent,
