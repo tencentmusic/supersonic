9d197407ec6cd5fb00e05c19c76c44415d27f4cc
williamhliu <williamhliu@tencent.com>
2023-11-10 17:17:40 +0800

    (feature)(chat-sdk) compatible with the selectedParses field

--- webapp/packages/chat-sdk/src/components/ChatItem/index.tsx
+++ webapp/packages/chat-sdk/src/components/ChatItem/index.tsx
@@ -151,23 +151,21 @@ const ChatItem: React.FC<Props> = ({
     setParseLoading(false);
     const { code, data } = parseData || {};
     const { state, selectedParses, candidateParses, queryId, parseTimeCost } = data || {};
+    const parses = selectedParses?.concat(candidateParses || []) || [];
     if (
       code !== 200 ||
       state === ParseStateEnum.FAILED ||
-      !selectedParses?.length ||
-      (!selectedParses[0]?.properties?.type && !selectedParses[0]?.queryMode)
+      !parses.length ||
+      (!parses[0]?.properties?.type && !parses[0]?.queryMode)
     ) {
       setParseTip(PARSE_ERROR_TIP);
       return;
     }
     onUpdateMessageScroll?.();
-    const parseInfos = selectedParses
-      .concat(candidateParses || [])
-      .slice(0, 5)
-      .map((item: any) => ({
-        ...item,
-        queryId,
-      }));
+    const parseInfos = parses.slice(0, 5).map((item: any) => ({
+      ...item,
+      queryId,
+    }));
     setParseInfoOptions(parseInfos || []);
     const parseInfoValue = parseInfos[0];
     setParseInfo(parseInfoValue);
