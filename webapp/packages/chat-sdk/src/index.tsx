3b2966673a8fba01aa9702d0e5f6ca25a630d635
williamhliu <williamhliu@tencent.com>
2023-12-15 17:40:22 +0800

    (feature)(chat-sdk) modify the method for obtaining similar questions and recommended drill-down dimensions; do not display the smart assistant button when there is only one assistant

--- webapp/packages/chat-sdk/src/index.tsx
+++ webapp/packages/chat-sdk/src/index.tsx
@@ -1,11 +1,11 @@
 import './styles/index.less';
 
-import ReactDOM from 'react-dom/client';
-import Chat from './demo/Chat';
-import ChatDemo from './demo/ChatDemo';
-import CopilotDemo from './demo/CopilotDemo';
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<ChatDemo />);
+// import ReactDOM from 'react-dom/client';
+// import Chat from './demo/Chat';
+// import ChatDemo from './demo/ChatDemo';
+// import CopilotDemo from './demo/CopilotDemo';
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(<ChatDemo />);
 
 export { default as Chat } from './Chat';
 
