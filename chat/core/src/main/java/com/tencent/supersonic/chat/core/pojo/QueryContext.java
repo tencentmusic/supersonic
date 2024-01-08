ad8079e058dfa271d4a6ee64af95dea3d1e9f3ca
lexluo09 <39718951+lexluo09@users.noreply.github.com>
2024-01-08 19:35:28 +0800

    (improvement)(chat) The system settings module supports setting the number of returned parses. (#606)

--- chat/core/src/main/java/com/tencent/supersonic/chat/core/pojo/QueryContext.java
+++ chat/core/src/main/java/com/tencent/supersonic/chat/core/pojo/QueryContext.java
@@ -7,11 +7,15 @@ import com.tencent.supersonic.chat.api.pojo.request.QueryFilters;
 import com.tencent.supersonic.chat.api.pojo.request.QueryReq;
 import com.tencent.supersonic.chat.api.pojo.response.ChatConfigRichResp;
 import com.tencent.supersonic.chat.core.agent.Agent;
+import com.tencent.supersonic.chat.core.config.OptimizationConfig;
 import com.tencent.supersonic.chat.core.plugin.Plugin;
 import com.tencent.supersonic.chat.core.query.SemanticQuery;
+import com.tencent.supersonic.common.util.ContextUtils;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,4 +36,15 @@ public class QueryContext {
     private Agent agent;
     private Map<Long, ChatConfigRichResp> modelIdToChatRichConfig;
     private Map<String, Plugin> nameToPlugin;
+
+    public List<SemanticQuery> getCandidateQueries() {
+        OptimizationConfig optimizationConfig = ContextUtils.getBean(OptimizationConfig.class);
+        Integer parseShowCount = optimizationConfig.getParseShowCount();
+        candidateQueries = candidateQueries.stream()
+                .sorted(Comparator.comparing(semanticQuery -> semanticQuery.getParseInfo().getScore(),
+                        Comparator.reverseOrder()))
+                .limit(parseShowCount)
+                .collect(Collectors.toList());
+        return candidateQueries;
+    }
 }
