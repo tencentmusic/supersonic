1e93282c9f194012547e5ecbc1e1cbc08a1a08c3
mainmain <57514971+mainmainer@users.noreply.github.com>
2024-01-10 22:01:21 +0800

    [improvement] llm supports all models (#615)

--- chat/core/src/main/java/com/tencent/supersonic/chat/core/query/llm/s2sql/LLMSqlResp.java
+++ chat/core/src/main/java/com/tencent/supersonic/chat/core/query/llm/s2sql/LLMSqlResp.java
@@ -2,11 +2,17 @@ package com.tencent.supersonic.chat.core.query.llm.s2sql;
 
 import java.util.List;
 import java.util.Map;
-import lombok.Builder;
+
 import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LLMSqlResp {
 
     private double sqlWeight;
