42a6f614568139fbc7b80c1c2167c09e2180f085
LXW <1264174498@qq.com>
2024-01-23 11:45:15 +0800

    (improvement)(chat) Provide agent tool type list (#683)
    Co-authored-by: jolunoluo

--- chat/server/src/main/java/com/tencent/supersonic/chat/server/rest/AgentController.java
+++ chat/server/src/main/java/com/tencent/supersonic/chat/server/rest/AgentController.java
@@ -3,10 +3,8 @@ package com.tencent.supersonic.chat.server.rest;
 import com.tencent.supersonic.auth.api.authentication.pojo.User;
 import com.tencent.supersonic.auth.api.authentication.utils.UserHolder;
 import com.tencent.supersonic.chat.core.agent.Agent;
+import com.tencent.supersonic.chat.core.agent.AgentToolType;
 import com.tencent.supersonic.chat.server.service.AgentService;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +12,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping({"/api/chat/agent", "/openapi/chat/agent"})
@@ -52,6 +54,11 @@ public class AgentController {
     @RequestMapping("/getAgentList")
     public List<Agent> getAgentList() {
         return agentService.getAgents();
+    }
+
+    @RequestMapping("/getToolTypes")
+    public Map<AgentToolType, String> getToolTypes() {
+        return AgentToolType.getToolTypes();
     }
 
 }
