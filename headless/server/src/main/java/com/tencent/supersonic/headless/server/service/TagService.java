8650817b919565e50f198e9dc465dbbea0f96a22
kanedai <kanedai@tencent.com>
2024-02-27 20:32:39 +0800

    [improvement](headless) add queryTag for tagMarket

--- headless/server/src/main/java/com/tencent/supersonic/headless/server/service/TagService.java
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/service/TagService.java
@@ -1,9 +1,12 @@
 package com.tencent.supersonic.headless.server.service;
 
+import com.github.pagehelper.PageInfo;
 import com.tencent.supersonic.auth.api.authentication.pojo.User;
 import com.tencent.supersonic.headless.api.pojo.request.TagReq;
 import com.tencent.supersonic.headless.api.pojo.response.TagResp;
 import com.tencent.supersonic.headless.server.pojo.TagFilter;
+import com.tencent.supersonic.headless.server.pojo.TagFilterPage;
+
 import java.util.List;
 
 public interface TagService {
@@ -14,7 +17,9 @@ public interface TagService {
 
     void delete(Long id, User user) throws Exception;
 
-    TagResp getMetric(Long id);
+    TagResp getTag(Long id);
 
     List<TagResp> query(TagFilter tagFilter);
+
+    PageInfo<TagResp> queryPage(TagFilterPage tagFilterPage, User user);
 }
