a76d882d58271d47574416a39d38f0eca1ba98a2
jipengli <jipengli@tencent.com>
2024-02-27 15:58:43 +0800

    (feature)(headless) Add tag query api

---
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/request/QueryTagReq.java
@@ -0,0 +1,64 @@
+package com.tencent.supersonic.headless.api.pojo.request;
+
+import com.google.common.collect.Lists;
+import com.tencent.supersonic.common.pojo.Aggregator;
+import com.tencent.supersonic.common.pojo.DateConf;
+import com.tencent.supersonic.common.pojo.Filter;
+import com.tencent.supersonic.common.pojo.Order;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+
+@Data
+@Slf4j
+@ToString
+public class QueryTagReq extends SemanticQueryReq {
+
+    private List<String> groups = new ArrayList<>();
+    private List<Aggregator> aggregators = new ArrayList<>();
+    private List<Filter> tagFilters = new ArrayList<>();
+    private List<Order> orders = new ArrayList<>();
+
+    private Long limit = 20L;
+    private Long offset = 0L;
+
+    private String tagFiltersDate;
+    private DateConf dateInfo;
+
+    @Override
+    public String toCustomizedString() {
+        StringBuilder stringBuilder = new StringBuilder("{");
+        stringBuilder.append("\"viewId\":")
+                .append(viewId);
+        stringBuilder.append("\"modelIds\":")
+                .append(modelIds);
+        stringBuilder.append(",\"groups\":")
+                .append(groups);
+        stringBuilder.append(",\"aggregators\":")
+                .append(aggregators);
+        stringBuilder.append(",\"orders\":")
+                .append(orders);
+        stringBuilder.append(",\"tagFilters\":")
+                .append(tagFilters);
+        stringBuilder.append(",\"dateInfo\":")
+                .append(dateInfo);
+        stringBuilder.append(",\"params\":")
+                .append(params);
+        stringBuilder.append(",\"limit\":")
+                .append(limit);
+        stringBuilder.append('}');
+        return stringBuilder.toString();
+    }
+
+    public List<String> getMetrics() {
+        List<String> metrics = Lists.newArrayList();
+        if (!CollectionUtils.isEmpty(this.aggregators)) {
+            metrics = aggregators.stream().map(Aggregator::getColumn).collect(Collectors.toList());
+        }
+        return metrics;
+    }
+}
--- headless/server/src/main/java/com/tencent/supersonic/headless/server/rest/QueryController.java
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/rest/QueryController.java
@@ -13,6 +13,7 @@ import com.tencent.supersonic.headless.api.pojo.request.QueryItemReq;
 import com.tencent.supersonic.headless.api.pojo.request.QueryMultiStructReq;
 import com.tencent.supersonic.headless.api.pojo.request.QuerySqlReq;
 import com.tencent.supersonic.headless.api.pojo.request.QueryStructReq;
+import com.tencent.supersonic.headless.api.pojo.request.QueryTagReq;
 import com.tencent.supersonic.headless.api.pojo.response.ExplainResp;
 import com.tencent.supersonic.headless.api.pojo.response.ItemQueryResultResp;
 import com.tencent.supersonic.headless.api.pojo.response.ItemUseResp;
@@ -56,6 +57,14 @@ public class QueryController {
         User user = UserHolder.findUser(request, response);
         QuerySqlReq querySqlReq = queryStructReq.convert(true);
         return queryService.queryByReq(querySqlReq, user);
+    }
+
+    @PostMapping("/tag")
+    public Object queryByTag(@RequestBody QueryTagReq queryTagReq,
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        User user = UserHolder.findUser(request, response);
+        return queryService.queryByReq(queryTagReq, user);
     }
 
     @PostMapping("/queryMetricDataById")
