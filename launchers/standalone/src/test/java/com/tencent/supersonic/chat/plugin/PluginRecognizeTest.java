43d231a4727f0fa4a302246e1cb8496a9e969c67
lexluo <lexluo@tencent.com>
2024-02-29 10:38:53 +0800

    (improvement)(chat) Fixes testing and compile issues

--- launchers/standalone/src/test/java/com/tencent/supersonic/chat/plugin/PluginRecognizeTest.java
+++ launchers/standalone/src/test/java/com/tencent/supersonic/chat/plugin/PluginRecognizeTest.java
@@ -57,7 +57,7 @@ public class PluginRecognizeTest extends BasePluginTest {
         QueryFilter queryFilter = new QueryFilter();
         queryFilter.setElementID(2L);
         queryFilter.setValue("alice");
-        queryRequest.setModelId(1L);
+        queryRequest.setViewId(1L);
         queryFilters.getFilters().add(queryFilter);
         queryRequest.setQueryFilters(queryFilters);
 
