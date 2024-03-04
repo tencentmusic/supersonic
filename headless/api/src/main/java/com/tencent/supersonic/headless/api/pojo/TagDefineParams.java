ba83c6ca81b2341a8663566c158137cc4f377a1d
daikon <1059907724@qq.com>
2024-03-04 16:42:21 +0800

    [improvement](headless) opt getNature (#783)

--- headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/TagDefineParams.java
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/TagDefineParams.java
@@ -2,8 +2,11 @@ package com.tencent.supersonic.headless.api.pojo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class TagDefineParams {
 
     private String expr;
+    private List<Object> dependencies;
 }
