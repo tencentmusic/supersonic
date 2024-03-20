61843552eab78474af5ee5a962fdcf70ca024778
lexluo <lexluo@tencent.com>
2024-03-20 15:16:26 +0800

    (improvement)(Headless) initializing by adding tag data.

---
+++ launchers/standalone/src/test/java/com/tencent/supersonic/headless/QueryTagValueTest.java
@@ -0,0 +1,26 @@
+package com.tencent.supersonic.headless;
+
+import static org.junit.Assert.assertTrue;
+
+import com.tencent.supersonic.auth.api.authentication.pojo.User;
+import com.tencent.supersonic.headless.api.pojo.SchemaElementType;
+import com.tencent.supersonic.headless.api.pojo.request.ItemValueReq;
+import com.tencent.supersonic.headless.api.pojo.response.ItemValueResp;
+import com.tencent.supersonic.headless.server.service.TagQueryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class QueryTagValueTest extends BaseTest {
+
+    @Autowired
+    protected TagQueryService tagQueryService;
+
+    @Test
+    public void testQueryTagValue() throws Exception {
+        ItemValueReq itemValueReq = new ItemValueReq();
+        itemValueReq.setItemId(1L);
+        itemValueReq.setType(SchemaElementType.DIMENSION);
+        ItemValueResp itemValueResp = tagQueryService.queryTagValue(itemValueReq, User.getFakeUser());
+        assertTrue(itemValueResp != null);
+    }
+}
