53178b5066319e26ebc0d1f60e08f6448f891cdd
LXW <1264174498@qq.com>
2024-03-21 14:11:00 +0800

    (improvement)(Chat) fix QueryFilter putting element (#848)
    Co-authored-by: jolunoluo

--- chat/server/src/main/java/com/tencent/supersonic/chat/server/parser/NL2SQLParser.java
+++ chat/server/src/main/java/com/tencent/supersonic/chat/server/parser/NL2SQLParser.java
@@ -3,9 +3,11 @@ package com.tencent.supersonic.chat.server.parser;
 import com.tencent.supersonic.chat.server.pojo.ChatParseContext;
 import com.tencent.supersonic.chat.server.util.QueryReqConverter;
 import com.tencent.supersonic.common.util.ContextUtils;
+import com.tencent.supersonic.headless.api.pojo.SemanticParseInfo;
 import com.tencent.supersonic.headless.api.pojo.request.QueryReq;
 import com.tencent.supersonic.headless.api.pojo.response.ParseResp;
 import com.tencent.supersonic.headless.server.service.ChatQueryService;
+import java.util.List;
 
 public class NL2SQLParser implements ChatParser {
 
@@ -14,12 +16,25 @@ public class NL2SQLParser implements ChatParser {
         if (!chatParseContext.enableNL2SQL()) {
             return;
         }
+        if (checkSkip(parseResp)) {
+            return;
+        }
         QueryReq queryReq = QueryReqConverter.buildText2SqlQueryReq(chatParseContext);
         ChatQueryService chatQueryService = ContextUtils.getBean(ChatQueryService.class);
         ParseResp text2SqlParseResp = chatQueryService.performParsing(queryReq);
         if (!ParseResp.ParseState.FAILED.equals(text2SqlParseResp.getState())) {
             parseResp.getSelectedParses().addAll(text2SqlParseResp.getSelectedParses());
         }
+    }
+
+    private boolean checkSkip(ParseResp parseResp) {
+        List<SemanticParseInfo> selectedParses = parseResp.getSelectedParses();
+        for (SemanticParseInfo semanticParseInfo : selectedParses) {
+            if (semanticParseInfo.getScore() >= parseResp.getQueryText().length()) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
