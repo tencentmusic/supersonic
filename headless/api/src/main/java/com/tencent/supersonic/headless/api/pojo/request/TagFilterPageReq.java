90607cd9d6b2578d6d77d4f2a91cd5e26e6c47b9
daikon <1059907724@qq.com>
2024-03-25 10:48:11 +0800

    (improvement)(headless) opt queryTagMarketPage logic, support query without tagObjectId (#854)

--- headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/request/TagFilterPageReq.java
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/request/TagFilterPageReq.java
@@ -3,12 +3,9 @@ package com.tencent.supersonic.headless.api.pojo.request;
 import com.tencent.supersonic.headless.api.pojo.enums.TagDefineType;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 public class TagFilterPageReq extends PageSchemaItemReq {
 
-    @NotNull
     private Long tagObjectId;
 
     private TagDefineType tagDefineType;
