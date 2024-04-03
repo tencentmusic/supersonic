6b60bfe6bcd4b5d0ae6fe23a9176977846a47830
LXW <1264174498@qq.com>
2024-04-03 20:36:49 +0800

    (improvement)(Headless) Fix model list auth checking (#883)
    * (improvement)(Headless) Fix model list auth check
    
    * (improvement)(Headless) Fix row permission
    
    ---------
    
    Co-authored-by: jolunoluo

--- headless/server/src/main/java/com/tencent/supersonic/headless/server/service/impl/ModelServiceImpl.java
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/service/impl/ModelServiceImpl.java
@@ -306,10 +306,15 @@ public class ModelServiceImpl implements ModelService {
         List<Long> domainIds = domainService.getDomainAuthSet(user, authType)
                 .stream().map(DomainResp::getId)
                 .collect(Collectors.toList());
-        if (domainIds.contains(domainId)) {
-            domainIds = Lists.newArrayList(domainId);
-        } else {
+        if (CollectionUtils.isEmpty(domainIds)) {
             return Lists.newArrayList();
+        }
+        if (domainId != null) {
+            if (domainIds.contains(domainId)) {
+                domainIds = Lists.newArrayList(domainId);
+            } else {
+                return Lists.newArrayList();
+            }
         }
         ModelFilter modelFilter = new ModelFilter();
         modelFilter.setIncludesDetail(false);
