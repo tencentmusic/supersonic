008c1c35d8956d48764e2305bcb8a060c148a99b
jerryjzhang <jerryjzhang@tencent.com>
2024-06-05 10:33:04 +0800

    (improvement)(headless)Commit new impl of SqlGenStrategy

--- headless/core/src/main/java/com/tencent/supersonic/headless/core/chat/parser/llm/SqlGenStrategy.java
+++ headless/core/src/main/java/com/tencent/supersonic/headless/core/chat/parser/llm/SqlGenStrategy.java
@@ -3,7 +3,6 @@ package com.tencent.supersonic.headless.core.chat.parser.llm;
 import com.tencent.supersonic.headless.api.pojo.LLMConfig;
 import com.tencent.supersonic.headless.core.chat.query.llm.s2sql.LLMReq;
 import com.tencent.supersonic.headless.core.chat.query.llm.s2sql.LLMResp;
-import com.tencent.supersonic.headless.core.config.ParserConfig;
 import com.tencent.supersonic.headless.core.utils.S2ChatModelProvider;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.slf4j.Logger;
@@ -22,13 +21,7 @@ public abstract class SqlGenStrategy implements InitializingBean {
     protected static final Logger keyPipelineLog = LoggerFactory.getLogger("keyPipeline");
 
     @Autowired
-    protected ExemplarManager exemplarManager;
-
-    @Autowired
-    protected ParserConfig parserConfig;
-
-    @Autowired
-    protected PromptGenerator promptGenerator;
+    protected PromptHelper promptHelper;
 
     protected ChatLanguageModel getChatLanguageModel(LLMConfig llmConfig) {
         return S2ChatModelProvider.provide(llmConfig);
