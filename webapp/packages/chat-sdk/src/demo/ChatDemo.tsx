81b9e4b109752b63ac002bd2f2cbcc9c22672b1d
方洋 <fangyang@cvte.com>
2024-06-07 18:02:35 +0800

    feat(chat-sdk): 正在响应中不允许发送消息

--- webapp/packages/chat-sdk/src/demo/ChatDemo.tsx
+++ webapp/packages/chat-sdk/src/demo/ChatDemo.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const ChatDemo: React.FC<Props> = ({}) => {
   return (
     <div className={styles.chatDemo}>
-      <Chat isDeveloper token='eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl91c2VyX2lkIjo4ODcsInRva2VuX3VzZXJfZGlzcGxheV9uYW1lIjoi5pa55rSLIiwidG9rZW5fY3JlYXRlX3RpbWUiOjE3MTc3MjA3NTA2NTIsInN1YiI6ImZhbmd5YW5nIiwidG9rZW5faXNfYWRtaW4iOjEsInRva2VuX3VzZXJfbmFtZSI6ImZhbmd5YW5nIiwiZXhwIjoxNzE3NzQyMzUwLCJ0b2tlbl91c2VyX3Bhc3N3b3JkIjoiZmFuZ3lhbmcifQ.m8tWb7NDEynuV6IUmF86_yTU0fW_UOF_9_Gdh9flajWmc_KnKKpYQWGBypuaKVWs1afl2zYXwpToRULo-qBc8w' />
+      <Chat isDeveloper token='eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl91c2VyX2lkIjo4ODcsInRva2VuX3VzZXJfZGlzcGxheV9uYW1lIjoi5pa55rSLIiwidG9rZW5fY3JlYXRlX3RpbWUiOjE3MTc3NDU2MjYyOTQsInN1YiI6ImZhbmd5YW5nIiwidG9rZW5faXNfYWRtaW4iOjEsInRva2VuX3VzZXJfbmFtZSI6ImZhbmd5YW5nIiwiZXhwIjoxNzE3NzY3MjI2LCJ0b2tlbl91c2VyX3Bhc3N3b3JkIjoiZmFuZ3lhbmcifQ.zErOiBDrScrvq2PTfWRO8pAPVM9vheocBO8JxKaz8DpAI7LQFB-HhXISwzNsvtpk71GwFjPyklD5vpUF9nU4LQ' />
     </div>
   );
 };
