33eecc0cb1039900a2ed7053b03285edf2fd0fcc
lexluo09 <39718951+lexluo09@users.noreply.github.com>
2024-06-11 23:13:40 +0800

    (improvement)(project) Fixed the issue of Swagger UI not working. (#1133)

--- launchers/standalone/src/main/java/com/tencent/supersonic/StandaloneLauncher.java
+++ launchers/standalone/src/main/java/com/tencent/supersonic/StandaloneLauncher.java
@@ -8,12 +8,14 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.tencent.supersonic"},
         exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableScheduling
 @EnableAsync
 @Import(S2LangChain4jAutoConfiguration.class)
+@EnableSwagger2
 public class StandaloneLauncher {
 
     public static void main(String[] args) {
