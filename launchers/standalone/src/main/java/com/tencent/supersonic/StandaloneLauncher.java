3831f9fdf144b436232bf982fac98b639b58cc0f
lexluo09 <39718951+lexluo09@users.noreply.github.com>
2024-06-20 20:27:06 +0800

    (improvement)(common) support qianfan in springboot (#1175)

--- launchers/standalone/src/main/java/com/tencent/supersonic/StandaloneLauncher.java
+++ launchers/standalone/src/main/java/com/tencent/supersonic/StandaloneLauncher.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = {"com.tencent.supersonic", "dev.langchain4j.model"},
+@SpringBootApplication(scanBasePackages = {"com.tencent.supersonic", "dev.langchain4j"},
         exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableScheduling
 @EnableAsync
