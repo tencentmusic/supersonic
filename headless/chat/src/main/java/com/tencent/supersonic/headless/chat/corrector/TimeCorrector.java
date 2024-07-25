4fba45df608641a183e897368284445882b70952
liangjj168 <liangjj168@annto.com.cn>
2024-07-25 15:55:46 +0800

    解决必须需要加日期的bug，改成llm不返回带’数据日期’修正的sql就不加日期查询

--- headless/chat/src/main/java/com/tencent/supersonic/headless/chat/corrector/TimeCorrector.java
+++ headless/chat/src/main/java/com/tencent/supersonic/headless/chat/corrector/TimeCorrector.java
@@ -1,21 +1,19 @@
 package com.tencent.supersonic.headless.chat.corrector;
 
 
-import com.tencent.supersonic.common.pojo.enums.TimeDimensionEnum;
-import com.tencent.supersonic.common.util.ContextUtils;
+import com.tencent.supersonic.common.jsqlparser.DateVisitor.DateBoundInfo;
 import com.tencent.supersonic.common.jsqlparser.SqlAddHelper;
 import com.tencent.supersonic.common.jsqlparser.SqlDateSelectHelper;
-import com.tencent.supersonic.common.jsqlparser.SqlSelectHelper;
 import com.tencent.supersonic.common.jsqlparser.SqlRemoveHelper;
-import com.tencent.supersonic.common.jsqlparser.DateVisitor.DateBoundInfo;
+import com.tencent.supersonic.common.jsqlparser.SqlSelectHelper;
+import com.tencent.supersonic.common.pojo.enums.TimeDimensionEnum;
+import com.tencent.supersonic.common.util.ContextUtils;
 import com.tencent.supersonic.headless.api.pojo.SemanticParseInfo;
 import com.tencent.supersonic.headless.chat.ChatQueryContext;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.core.env.Environment;
 import org.springframework.util.CollectionUtils;
 
@@ -66,24 +64,9 @@ public class TimeCorrector extends BaseSemanticCorrector {
         if (StringUtils.isNotBlank(correctorDate) && !Boolean.parseBoolean(correctorDate)) {
             return;
         }
+        // if llm return not has date info ,then it will stop
         if (CollectionUtils.isEmpty(whereFields) || !TimeDimensionEnum.containsZhTimeDimension(whereFields)) {
-
-            Pair<String, String> startEndDate = S2SqlDateHelper.getStartEndDate(chatQueryContext,
-                    semanticParseInfo.getDataSetId(), semanticParseInfo.getQueryType());
-
-            if (StringUtils.isNotBlank(startEndDate.getLeft())
-                    && StringUtils.isNotBlank(startEndDate.getRight())) {
-                correctS2SQL = SqlAddHelper.addParenthesisToWhere(correctS2SQL);
-                String dateChName = TimeDimensionEnum.DAY.getChName();
-                String condExpr = String.format(" ( %s >= '%s'  and %s <= '%s' )", dateChName,
-                        startEndDate.getLeft(), dateChName, startEndDate.getRight());
-                try {
-                    Expression expression = CCJSqlParserUtil.parseCondExpression(condExpr);
-                    correctS2SQL = SqlAddHelper.addWhere(correctS2SQL, expression);
-                } catch (JSQLParserException e) {
-                    log.error("parseCondExpression:{}", e);
-                }
-            }
+            return;
         }
         semanticParseInfo.getSqlInfo().setCorrectedS2SQL(correctS2SQL);
     }
