97bf8049d73022ed165a09ebe7b84bb62b824728
LXW <1264174498@qq.com>
2024-08-03 23:56:29 +0800

    (improvement)(headless) Divide dimensions into four types: identify, category, time and partition time (#1509)
    Co-authored-by: lxwcodemonkey

---
+++ common/src/main/java/com/tencent/supersonic/common/pojo/DimensionConstants.java
@@ -0,0 +1,9 @@
+package com.tencent.supersonic.common.pojo;
+
+public class DimensionConstants {
+
+    public static final String DIMENSION_TIME_FORMAT = "time_format";
+
+    public static final String DIMENSION_TYPE = "dimension_type";
+
+}
--- headless/server/src/main/java/com/tencent/supersonic/headless/server/utils/ModelConverter.java
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/utils/ModelConverter.java
@@ -3,12 +3,10 @@ package com.tencent.supersonic.headless.server.utils;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.tencent.supersonic.auth.api.authentication.pojo.User;
+import com.tencent.supersonic.common.pojo.DimensionConstants;
 import com.tencent.supersonic.common.pojo.enums.StatusEnum;
 import com.tencent.supersonic.common.util.BeanMapper;
 import com.tencent.supersonic.common.util.JsonUtil;
-import com.tencent.supersonic.headless.api.pojo.enums.DimensionType;
-import com.tencent.supersonic.headless.api.pojo.enums.MetricDefineType;
-import com.tencent.supersonic.headless.api.pojo.enums.SemanticType;
 import com.tencent.supersonic.headless.api.pojo.Dim;
 import com.tencent.supersonic.headless.api.pojo.DrillDownDimension;
 import com.tencent.supersonic.headless.api.pojo.Identify;
@@ -16,6 +14,9 @@ import com.tencent.supersonic.headless.api.pojo.Measure;
 import com.tencent.supersonic.headless.api.pojo.MeasureParam;
 import com.tencent.supersonic.headless.api.pojo.MetricDefineByMeasureParams;
 import com.tencent.supersonic.headless.api.pojo.ModelDetail;
+import com.tencent.supersonic.headless.api.pojo.enums.DimensionType;
+import com.tencent.supersonic.headless.api.pojo.enums.MetricDefineType;
+import com.tencent.supersonic.headless.api.pojo.enums.SemanticType;
 import com.tencent.supersonic.headless.api.pojo.request.DimensionReq;
 import com.tencent.supersonic.headless.api.pojo.request.MetricReq;
 import com.tencent.supersonic.headless.api.pojo.request.ModelReq;
@@ -29,6 +30,7 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -108,14 +110,17 @@ public class ModelConverter {
         dimensionReq.setName(dim.getName());
         dimensionReq.setBizName(dim.getBizName());
         dimensionReq.setDescription(dim.getName());
-        if (Objects.equals(dim.getType(), DimensionType.time.name())) {
+        if (DimensionType.isTimeDimension(dim.getType())) {
             dimensionReq.setSemanticType(SemanticType.DATE.name());
+            Map<String, Object> map = new HashMap<>();
+            map.put(DimensionConstants.DIMENSION_TIME_FORMAT, dim.getDateFormat());
+            dimensionReq.setExt(map);
         } else {
             dimensionReq.setSemanticType(SemanticType.CATEGORY.name());
         }
         dimensionReq.setModelId(modelDO.getId());
         dimensionReq.setExpr(dim.getBizName());
-        dimensionReq.setType(DimensionType.categorical.name());
+        dimensionReq.setType(dim.getType());
         dimensionReq.setDescription(Objects.isNull(dim.getDescription()) ? "" : dim.getDescription());
         dimensionReq.setIsTag(dim.getIsTag());
         return dimensionReq;
@@ -142,14 +147,10 @@ public class ModelConverter {
         dimensionReq.setName(identify.getName());
         dimensionReq.setBizName(identify.getBizName());
         dimensionReq.setDescription(identify.getName());
-        if (Objects.equals(identify.getType(), DimensionType.time.name())) {
-            dimensionReq.setSemanticType(SemanticType.DATE.name());
-        } else {
-            dimensionReq.setSemanticType(SemanticType.CATEGORY.name());
-        }
+        dimensionReq.setSemanticType(SemanticType.CATEGORY.name());
         dimensionReq.setModelId(modelDO.getId());
         dimensionReq.setExpr(identify.getBizName());
-        dimensionReq.setType(identify.getType());
+        dimensionReq.setType(DimensionType.identify.name());
         return dimensionReq;
     }
 
