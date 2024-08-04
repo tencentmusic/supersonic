6b93e5af6be490f03ebc5cea3ae968a101b8361e
lexluo <lexluo@tencent.com>
2024-08-04 22:21:54 +0800

    (improvement)(chat) Enable partition dates in the demo and support partition dates at the dataset granularity.

--- launchers/standalone/src/test/java/com/tencent/supersonic/chat/TagTest.java
+++ launchers/standalone/src/test/java/com/tencent/supersonic/chat/TagTest.java
@@ -33,7 +33,7 @@ public class TagTest extends BaseTest {
         expectedParseInfo.setAggType(AggregateTypeEnum.NONE);
 
         QueryFilter dimensionFilter = DataUtils.getFilter("genre", FilterOperatorEnum.EQUALS,
-                "流行", "风格", 6L);
+                "流行", "风格", 7L);
         expectedParseInfo.getDimensionFilters().add(dimensionFilter);
 
         SchemaElement metric = SchemaElement.builder().name("播放量").build();
