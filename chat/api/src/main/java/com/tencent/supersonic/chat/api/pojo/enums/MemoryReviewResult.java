0c3a609eed40d159b5acbee3ee03948c5619d59b
xiuzhu <edy@dodge-pro.local>
2024-08-26 09:54:35 +0800

    Code format class MemoryReviewResult

--- chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/enums/MemoryReviewResult.java
+++ chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/enums/MemoryReviewResult.java
@@ -16,6 +16,6 @@ public enum MemoryReviewResult {
                 return reviewRet;
             }
         }
-        throw new InvalidArgumentException("Invalid MemoryReviewResult type:[" + value +"]");
+        throw new InvalidArgumentException("Invalid MemoryReviewResult type:[" + value + "]");
     }
 }
