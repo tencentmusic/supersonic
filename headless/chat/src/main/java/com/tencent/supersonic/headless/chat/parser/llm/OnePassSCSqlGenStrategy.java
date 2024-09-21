15610a35375d3d39b1ba7b2d02dc9f4d1facf5bd
lxwcodemonkey <jolunoluo@tencent.com>
2024-09-21 13:18:31 +0800

    (fix)(chat) fix the issue that front-end filter time re-query does not take effect, when the partition time field name is not imp_date #1638

--- headless/chat/src/main/java/com/tencent/supersonic/headless/chat/parser/llm/OnePassSCSqlGenStrategy.java
+++ headless/chat/src/main/java/com/tencent/supersonic/headless/chat/parser/llm/OnePassSCSqlGenStrategy.java
@@ -36,7 +36,8 @@ public class OnePassSCSqlGenStrategy extends SqlGenStrategy {
                     + "\n2.ALWAYS specify date filter using `>`,`<`,`>=`,`<=` operator."
                     + "\n3.DO NOT include date filter in the where clause if not explicitly expressed in the `Question`."
                     + "\n4.DO NOT calculate date range using functions."
-                    + "\n5.DO NOT miss the AGGREGATE operator of metrics, always add it as needed."
+                    + "\n5.DO NOT calculate date range using DATE_SUB."
+                    + "\n6.DO NOT miss the AGGREGATE operator of metrics, always add it as needed."
                     + "\n#Exemplars:\n{{exemplar}}"
                     + "#Question:\nQuestion:{{question}},Schema:{{schema}},SideInfo:{{information}}";
 
