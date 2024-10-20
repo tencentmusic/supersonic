f7e282eabd2e5d108348bf5293930f9ec2d044b6
dszhao <zhaodongsheng@cvte.com>
2024-10-20 17:00:25 +0800

    (feat) 1、add user access token; #1829; 2、support change password #824

--- webapp/packages/supersonic-fe/src/services/user.ts
+++ webapp/packages/supersonic-fe/src/services/user.ts
@@ -1,4 +1,5 @@
 import request from 'umi-request';
+import { API } from './API';
 
 export async function query() {
   return request<API.CurrentUser[]>(`${process.env.API_BASE_URL}users`);
@@ -18,5 +19,33 @@ export function saveSystemConfig(data: any): Promise<any> {
   return request(`${process.env.API_BASE_URL}parameter`, {
     method: 'post',
     data,
+  });
+}
+
+export function changePassword(data: { newPassword: string; oldPassword: string }): Promise<any> {
+  return request(`${process.env.AUTH_API_BASE_URL}user/resetPassword`, {
+    method: 'post',
+    data: {
+      newPassword: data.newPassword,
+      password: data.oldPassword,
+    },
+  });
+}
+
+// 获取用户accessTokens
+export async function getUserAccessTokens(): Promise<Result<API.UserItem[]>> {
+  return request.get(`${process.env.AUTH_API_BASE_URL}user/getUserTokens`);
+}
+
+export function generateAccessToken(data: { expireTime: number; name: string }): Promise<any> {
+  return request(`${process.env.AUTH_API_BASE_URL}user/generateToken`, {
+    method: 'post',
+    data,
+  });
+}
+
+export function removeAccessToken(id: number): Promise<any> {
+  return request(`${process.env.AUTH_API_BASE_URL}user/deleteUserToken?tokenId=${id}`, {
+    method: 'post',
   });
 }
