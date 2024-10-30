9644fc42072ed8cf05eafa372ed981b115321eaa
jerryjzhang <jerryjzhang@tencent.com>
2024-10-30 10:36:19 +0800

    [improvement][chat]Remove deprecated `SatisfactoryChecker`.

--- headless/chat/src/main/java/com/tencent/supersonic/headless/chat/parser/QueryTypeParser.java
+++ headless/chat/src/main/java/com/tencent/supersonic/headless/chat/parser/QueryTypeParser.java
@@ -23,7 +23,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/** QueryTypeParser resolves query type as either METRIC or TAG, or ID. */
+/** QueryTypeParser resolves query type as either AGGREGATE or DETAIL or ID. */
 @Slf4j
 public class QueryTypeParser implements SemanticParser {
 
