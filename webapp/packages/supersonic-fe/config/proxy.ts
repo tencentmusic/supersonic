9c70a36d40684141a1a7447488de082ae490a623
tristanliu <tristanliu@tencent.com>
2024-10-30 17:40:38 +0800

    [improvement][headless-be] rollback proxy config

--- webapp/packages/supersonic-fe/config/proxy.ts
+++ webapp/packages/supersonic-fe/config/proxy.ts
@@ -1,9 +1,7 @@
 export default {
   dev: {
     '/api/': {
-      // target: 'http://127.0.0.1:9080',
-      // target: 'http://10.91.206.64:9080',
-      target: 'https://supersonic-pre.tmeoa.com/',
+      target: 'http://127.0.0.1:9080',
       changeOrigin: true,
     },
   },
