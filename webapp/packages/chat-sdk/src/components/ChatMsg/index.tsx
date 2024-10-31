febd42e70c066dfea0b649807d1442be45a2b736
tristanliu <tristanliu@tencent.com>
2024-10-31 19:43:14 +0800

    [improvement][headless-fe] Enhanced the display of errMsg and textSummary in the question-answering dialogue of the assistant module

--- webapp/packages/chat-sdk/src/components/ChatMsg/index.tsx
+++ webapp/packages/chat-sdk/src/components/ChatMsg/index.tsx
@@ -214,8 +214,7 @@ const ChatMsg: React.FC<Props> = ({
         );
       case MsgContentTypeEnum.MARKDOWN:
         return (
-          <div style={{ maxHeight: 800, overflow: 'scroll' }}>
-            {/* {data.textResult} */}
+          <div style={{ maxHeight: 800 }}>
             <MarkDown markdown={data.textResult} loading={loading} />
           </div>
         );
