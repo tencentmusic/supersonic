3b65b1c80b94e4adc64696db7d4f4fcccf0f1cc2
lexluo09 <39718951+lexluo09@users.noreply.github.com>
2024-11-04 10:06:10 +0800

    [improvement][headless] Remove entities from the dictionary and search interface (#1878)

--- headless/chat/src/main/java/com/tencent/supersonic/headless/chat/mapper/SearchMatchStrategy.java
+++ headless/chat/src/main/java/com/tencent/supersonic/headless/chat/mapper/SearchMatchStrategy.java
@@ -1,13 +1,11 @@
 package com.tencent.supersonic.headless.chat.mapper;
 
 import com.google.common.collect.Lists;
-import com.tencent.supersonic.common.pojo.enums.DictWordType;
 import com.tencent.supersonic.headless.api.pojo.response.S2Term;
 import com.tencent.supersonic.headless.chat.ChatQueryContext;
 import com.tencent.supersonic.headless.chat.knowledge.HanlpMapResult;
 import com.tencent.supersonic.headless.chat.knowledge.KnowledgeBaseService;
 import com.tencent.supersonic.headless.chat.knowledge.SearchService;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +15,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 /**
  * SearchMatchStrategy encapsulates a concrete matching algorithm executed during search process.
@@ -66,16 +63,6 @@ public class SearchMatchStrategy extends BaseMatchStrategy<HanlpMapResult> {
                         knowledgeBaseService.suffixSearch(detectSegment, SEARCH_SIZE,
                                 chatQueryContext.getModelIdToDataSetIds(), detectDataSetIds);
                 hanlpMapResults.addAll(suffixHanlpMapResults);
-                // remove entity name where search
-                hanlpMapResults = hanlpMapResults.stream().filter(entry -> {
-                    List<String> natures = entry.getNatures().stream()
-                            .filter(nature -> !nature.endsWith(DictWordType.ENTITY.getType()))
-                            .collect(Collectors.toList());
-                    if (CollectionUtils.isEmpty(natures)) {
-                        return false;
-                    }
-                    return true;
-                }).collect(Collectors.toList());
                 MatchText matchText =
                         MatchText.builder().regText(regText).detectSegment(detectSegment).build();
                 regTextMap.put(matchText, hanlpMapResults);
