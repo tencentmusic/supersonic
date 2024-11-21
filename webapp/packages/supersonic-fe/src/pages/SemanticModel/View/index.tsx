e534bd3f2fb2919ee9ff6620a4210e9bbfc8d7a3
pisces <564779653@qq.com>
2024-11-21 19:00:40 +0800

    fix(semanticModel): model options not exist when creating new dataset

--- webapp/packages/supersonic-fe/src/pages/SemanticModel/View/index.tsx
+++ webapp/packages/supersonic-fe/src/pages/SemanticModel/View/index.tsx
@@ -3,14 +3,15 @@ import { ISemantic } from '../data';
 import DataSetTable from './components/DataSetTable';
 
 type Props = {
+  isCurrent: boolean;
   disabledEdit?: boolean;
   dataSetList: ISemantic.IDatasetItem[];
 };
 
-const View: React.FC<Props> = ({ dataSetList, disabledEdit = false }) => {
+const View: React.FC<Props> = ({ isCurrent, dataSetList, disabledEdit = false }) => {
   return (
     <div style={{ padding: '15px 20px' }}>
-      <DataSetTable disabledEdit={disabledEdit} dataSetList={dataSetList} />
+      <DataSetTable isCurrent={isCurrent} disabledEdit={disabledEdit} dataSetList={dataSetList} />
     </div>
   );
 };
