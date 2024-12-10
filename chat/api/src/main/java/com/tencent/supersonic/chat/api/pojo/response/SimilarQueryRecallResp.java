fca0817865f7344dbe5ff7849ec3754c5d01e991
wua.ming <wua.ming@meicloud.com>
2024-12-10 16:05:47 +0800

    (fix)(chat-api) 小范围整理一下lombok标签，增加默认构造函数避免高版本json框架无法实例化报错

--- chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/response/SimilarQueryRecallResp.java
+++ chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/response/SimilarQueryRecallResp.java
@@ -1,10 +1,14 @@
 package com.tencent.supersonic.chat.api.pojo.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimilarQueryRecallResp {
 
     private Long queryId;
