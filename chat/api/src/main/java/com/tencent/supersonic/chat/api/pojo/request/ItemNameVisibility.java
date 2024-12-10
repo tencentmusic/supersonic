848b4a1e44a559a3a1dc2ffed36e4a3beb7949ed
mislayming <18808893@qq.com>
2024-12-10 19:14:32 +0800

    (fix)(chat-api) 小范围整理一下lombok标签，增加默认构造函数避免高版本json框架无法实例化报错 (#1951)

--- chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ItemNameVisibility.java
+++ chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ItemNameVisibility.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.ToString;
 
 @Data
-@ToString
 public class ItemNameVisibility {
 
     private ItemNameVisibilityInfo aggVisibilityInfo;
--- chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ItemNameVisibilityInfo.java
+++ chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ItemNameVisibilityInfo.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@ToString
 public class ItemNameVisibilityInfo {
 
     /** invisible dimensions */
