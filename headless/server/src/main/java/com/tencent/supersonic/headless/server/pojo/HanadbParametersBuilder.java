af5c34a6a912ba158803ca218c101e53b181e858
wwsheng009 <vincentwwsheng@gmail.com>
2024-12-12 21:45:43 +0800

    (feature)(common) add sap hana db support

---
+++ headless/server/src/main/java/com/tencent/supersonic/headless/server/pojo/HanadbParametersBuilder.java
@@ -0,0 +1,16 @@
+package com.tencent.supersonic.headless.server.pojo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class HanadbParametersBuilder extends DefaultParametersBuilder {
+
+    @Override
+    public List<DatabaseParameter> build() {
+        return super.build();
+    }
+}
