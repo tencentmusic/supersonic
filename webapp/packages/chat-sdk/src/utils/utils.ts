988ac0d754f6e26b78e66263ccc3d94196b13524
williamhliu <williamhliu@tencent.com>
2025-02-18 19:20:04 +0800

    (feature)(chat-sdk) add unit

--- webapp/packages/chat-sdk/src/utils/utils.ts
+++ webapp/packages/chat-sdk/src/utils/utils.ts
@@ -93,7 +93,9 @@ export const getFormattedValue = (value: number | string, remainZero?: boolean) 
 
 export const formatNumberWithCN = (num: number) => {
   if (isNaN(num)) return '-';
-  if (num >= 10000) {
+  if (num >= 100000000) {
+    return (num / 100000000).toFixed(1) + '亿';
+  } else if (num >= 10000) {
     return (num / 10000).toFixed(1) + '万';
   } else {
     return formatByDecimalPlaces(num, 2);
