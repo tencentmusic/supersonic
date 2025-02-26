58e5803aa8787c13c1290c41adbce6ccfabb9f6d
iridescentpeo <chnliyufan@gmail.com>
2025-02-26 16:59:33 +0800

    新增数据解读接口

--- chat/server/src/main/java/com/tencent/supersonic/chat/server/rest/ChatQueryController.java
+++ chat/server/src/main/java/com/tencent/supersonic/chat/server/rest/ChatQueryController.java
@@ -5,6 +5,7 @@ import com.tencent.supersonic.chat.api.pojo.request.ChatExecuteReq;
 import com.tencent.supersonic.chat.api.pojo.request.ChatParseReq;
 import com.tencent.supersonic.chat.api.pojo.request.ChatQueryDataReq;
 import com.tencent.supersonic.chat.api.pojo.response.ChatParseResp;
+import com.tencent.supersonic.chat.api.pojo.response.QueryResult;
 import com.tencent.supersonic.chat.server.service.ChatQueryService;
 import com.tencent.supersonic.common.pojo.User;
 import com.tencent.supersonic.common.pojo.exception.InvalidArgumentException;
@@ -49,7 +50,12 @@ public class ChatQueryController {
         chatExecuteReq.setUser(UserHolder.findUser(request, response));
         return chatQueryService.execute(chatExecuteReq);
     }
-
+    @PostMapping("dataInterpret")
+    public Object dataInterpret(@RequestBody ChatExecuteReq chatExecuteReq, HttpServletRequest request,
+                          HttpServletResponse response) throws Exception {
+        chatExecuteReq.setUser(UserHolder.findUser(request, response));
+        return chatQueryService.dataInterpret(chatExecuteReq);
+    }
     @PostMapping("/")
     public Object query(@RequestBody ChatParseReq chatParseReq, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
