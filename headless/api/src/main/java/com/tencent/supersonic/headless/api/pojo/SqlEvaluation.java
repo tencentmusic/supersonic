11ff99cdbe72825a3c73f4ebb553dd51fa8bc7e9
zyclove <randeng.zhao@tuya.com>
2025-02-26 17:32:20 +0800

    fix: Caused by: java.io.NotSerializableException: DateConf、SchemaNameLengthComparator、SqlEvaluation and etc. (#2110)

--- headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/SqlEvaluation.java
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/SqlEvaluation.java
@@ -2,8 +2,11 @@ package com.tencent.supersonic.headless.api.pojo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class SqlEvaluation {
+public class SqlEvaluation implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Boolean isValidated;
     private String validateMsg;
