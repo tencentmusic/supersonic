a99f5985f594268165dc65ed0129c3b03f11a7e7
jerryjzhang <jerryjzhang@tencent.com>
2025-03-09 10:58:49 +0800

    (improvement)(headless)Set fields of model detail when restoring model.

--- headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/ModelDetail.java
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/ModelDetail.java
@@ -49,27 +49,4 @@ public class ModelDetail {
                 .collect(Collectors.toList());
     }
 
-    public List<Field> getFields() {
-        if (!CollectionUtils.isEmpty(fields)) {
-            return fields;
-        }
-        List<Field> fieldList = Lists.newArrayList();
-        // Compatible with older versions
-        if (!CollectionUtils.isEmpty(identifiers)) {
-            fieldList.addAll(identifiers.stream()
-                    .map(identify -> Field.builder().fieldName(identify.getFieldName()).build())
-                    .collect(Collectors.toSet()));
-        }
-        if (!CollectionUtils.isEmpty(dimensions)) {
-            fieldList.addAll(dimensions.stream()
-                    .map(dim -> Field.builder().fieldName(dim.getFieldName()).build())
-                    .collect(Collectors.toSet()));
-        }
-        if (!CollectionUtils.isEmpty(measures)) {
-            fieldList.addAll(measures.stream()
-                    .map(measure -> Field.builder().fieldName(measure.getFieldName()).build())
-                    .collect(Collectors.toSet()));
-        }
-        return fieldList;
-    }
 }
