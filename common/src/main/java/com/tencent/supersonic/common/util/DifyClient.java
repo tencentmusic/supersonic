25f486f16da21a4397b2a2dd2bbbf002c865a1d0
wanghaibo <wanghaibo@richinfo.cn>
2025-03-14 14:33:51 +0800

    支持dify流式输出

--- common/src/main/java/com/tencent/supersonic/common/util/DifyClient.java
+++ common/src/main/java/com/tencent/supersonic/common/util/DifyClient.java
@@ -1,11 +1,21 @@
 package com.tencent.supersonic.common.util;
 
 import lombok.extern.slf4j.Slf4j;
-
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import okhttp3.sse.EventSources;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
 
 @Slf4j
 public class DifyClient {
@@ -14,8 +24,12 @@ public class DifyClient {
 
     private String difyURL;
     private String difyKey;
+    
+    private final OkHttpClient okHttpClient;
 
     public DifyClient(String difyURL, String difyKey) {
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        this.okHttpClient = okHttpClientBuilder.build();
         this.difyURL = difyURL;
         this.difyKey = difyKey;
     }
@@ -58,6 +72,33 @@ public class DifyClient {
             log.error("请求dify失败---->" + e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+    
+    public void streamingGenerate(String prompt, String user, StreamingResponseHandler<AiMessage> handler) {
+        Map<String, String> headers = defaultHeaders();
+        DifyRequest request = new DifyRequest();
+        request.setQuery(prompt);
+        request.setUser(user);
+        request.setResponseMode("streaming");
+        EventSourceListener eventSourceListener = new EventSourceListener() {
+            
+            @Override
+            public void onEvent(EventSource eventSource, String id, String type, String data) {
+                JSONObject object = JSON.parseObject(data);
+                String event = object.getString("event");
+                if ("message".equals(event)) {
+                    handler.onNext(object.getString("answer"));
+                }
+            }
+            
+        };
+        Request.Builder builder = new Request.Builder();
+        builder.url(difyURL).headers(Headers.of(headers)).post(RequestBody.create(JSON.toJSONBytes(request)));
+        EventSources.createFactory(this.okHttpClient)
+            .newEventSource(
+                    builder.build(),
+                    eventSourceListener
+            );
     }
 
     public String parseSQLResult(String sql) {
