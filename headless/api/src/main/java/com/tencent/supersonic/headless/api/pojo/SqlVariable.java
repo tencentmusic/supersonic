0709575cd95d7b8e0c5a2c743c7dbf176203d5e6
wanglongqiang <wanglongqiangxxy@163.com>
2025-05-20 22:35:28 +0800

    when sql script has variables, generate sql can't be executed, this commit  supports variable replement (#2259)

--- headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/SqlVariable.java
+++ headless/api/src/main/java/com/tencent/supersonic/headless/api/pojo/SqlVariable.java
@@ -2,11 +2,17 @@ package com.tencent.supersonic.headless.api.pojo;
 
 import com.google.common.collect.Lists;
 import com.tencent.supersonic.headless.api.pojo.enums.VariableValueType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SqlVariable {
     private String name;
     private VariableValueType valueType;
