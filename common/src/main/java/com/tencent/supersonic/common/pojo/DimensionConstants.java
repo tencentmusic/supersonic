9af64994918ecae774d6bfeeee1b7f173f96300f
lwhy <72678624+luowanghaoyun@users.noreply.github.com>
2025-09-21 23:23:22 +0800

    (fix)(headless)Add dimension datatype in prompt (#2371)

--- common/src/main/java/com/tencent/supersonic/common/pojo/DimensionConstants.java
+++ common/src/main/java/com/tencent/supersonic/common/pojo/DimensionConstants.java
@@ -5,4 +5,6 @@ public class DimensionConstants {
     public static final String DIMENSION_TIME_FORMAT = "time_format";
 
     public static final String DIMENSION_TYPE = "dimension_type";
+
+    public static final String DIMENSION_DATA_TYPE = "dimension_data_type";
 }
